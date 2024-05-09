3c738e788dbac1d6dd674568da87b4e523e4f801
Tink Cai <Tink.Cai@dbschenker.com>
2024-05-09 11:21:33 +0800

    fix new version of cmd

--- client/client.js
+++ client/client.js
@@ -1,4 +1,4 @@
-const program = require('commander');
+const { program } = require('commander');
 const exec = require('child_process').exec;
 const fs = require('fs');
 const path = require('path');
